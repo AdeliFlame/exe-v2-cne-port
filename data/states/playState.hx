23de8c5d57dcb57325fb204d8b475d0e96ffe8a2
trianious <flareonvevo@gmail.com>
2024-02-19 13:35:22 -0600

    dfsa

--- data/states/playState.hx
+++ data/states/playState.hx
@@ -9,3 +9,12 @@ FlxG.cameras.remove(camHUD, false);
 FlxG.cameras.add(camHUD, false);
 FlxG.cameras.add(camHUD2, false);
 }
+
+function postUpdate() {
+    switch(strumLines.members[curCameraTarget].characters[0].getAnimName()) {
+        case "singLEFT": camFollow.x -= 15;
+        case "singDOWN": camFollow.y += 15;
+        case "singUP": camFollow.y -= 15;
+        case "singRIGHT": camFollow.x += 15;
+    }
+}
